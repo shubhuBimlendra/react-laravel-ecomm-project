ec96c43d538bf3d06bbdca580966386706899905
Bimlendra <bimlendrayadav2075@gmail.com>
2023-08-21 16:02:52 +0545

    Registraion API Implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Home from "./components/frontend/Home";
 import Login from "./components/frontend/auth/Login";
 import Register from "./components/frontend/auth/Register";
 
+//to generate csrf tokens
+import axios from "axios";
+axios.defaults.withCredentials = true;
+
+//base url
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+
 function App() {
   return (
     <div className="app">
--- src/components/frontend/auth/Register.js
+++ src/components/frontend/auth/Register.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../../layouts/frontend/Navbar";
+import axios from "axios";
+import swal from "sweetalert";
+import { useHistory } from "react-router-dom";
 
 function Register() {
+  const history = useHistory();
+  const [registerInput, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error_list: [],
+  });
+
+  //store
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({ ...registerInput, [e.target.name]: e.target.value });
+  };
+
+  const registerSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    };
+
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.post(`/api/register`, data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem("auth_token", res.data.token);
+          localStorage.setItem("auth_name", res.data.username);
+          swal("Success", res.data.message, "success");
+          history.push("/");
+        } else {
+          setRegister({
+            ...registerInput,
+            error_list: res.data.validation_errors,
+          });
+        }
+      });
+    });
+  };
+
   return (
     <div>
       <Navbar />
@@ -13,42 +55,42 @@ function Register() {
                 <h1>Register</h1>
               </div>
               <div className="card-body">
-                <form>
+                <form onSubmit={registerSubmit}>
                   <div className="form-group mb-3">
                     <label>Full Name</label>
                     <input
                       type="text"
                       name="name"
+                      onChange={handleInput}
+                      value={registerInput.name}
                       className="form-control"
                       placeholder="Enter Your Name"
                     />
+                    <span>{registerInput.error_list.name}</span>
                   </div>
                   <div className="form-group mb-3">
                     <label>Email</label>
                     <input
                       type="email"
                       name="email"
+                      onChange={handleInput}
+                      value={registerInput.email}
                       className="form-control"
                       placeholder="Enter Your Email"
                     />
+                    <span>{registerInput.error_list.email}</span>
                   </div>
                   <div className="form-group mb-3">
                     <label>Password</label>
                     <input
                       type="password"
                       name="password"
+                      onChange={handleInput}
+                      value={registerInput.password}
                       className="form-control"
                       placeholder="Enter Your Password"
                     />
-                  </div>
-                  <div className="form-group mb-3">
-                    <label>Confirm Password</label>
-                    <input
-                      type="password"
-                      name="confirm_password"
-                      className="form-control"
-                      placeholder="Enter Your Password"
-                    />
+                    <span>{registerInput.error_list.password}</span>
                   </div>
                   <div className="form-group mb-3">
                     <button type="submit" className="btn btn-primary">
